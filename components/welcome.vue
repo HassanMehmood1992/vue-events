8f2e0418cdaabfbdd63d052b7ef077d44806a1a6
Hassan Mehmood <hmehmood92@gmail.com>
2021-04-17 17:55:53 +0400

    welcome page updated

--- components/welcome.vue
+++ components/welcome.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-container
-    class="ma-0 pa-0"
-    :style="
-      `background:url(${$appURL}images/roadsurfer-campervan-hire.jpg); height:80vh;`
-    "
-    fluid
-  >
-    <v-layout row class="my-10" justify-end>
-      <v-flex md3 class="secondary pa-4 ma-3 title white--text">
-        #RoadSurferAssignment
-      </v-flex>
-    </v-layout>
+  <v-container class="ma-0 pa-0" fluid>
+    <v-img
+      :src="`${$appURL}images/roadsurfer-campervan-hire.jpg`"
+      :lazy-src="`${$appURL}images/roadsurfer-campervan-hire.jpg`"
+      min-height="50vh"
+    >
+      <v-layout row class="my-10" justify-end>
+        <v-flex md3 class="secondary pa-4 ma-3 title white--text">
+          #RoadSurferAssignment
+        </v-flex>
+      </v-layout>
+    </v-img>
   </v-container>
 </template>
 
